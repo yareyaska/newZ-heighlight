395ace6d9809cb237aad7a46de7dbe208c526826
yareyaska <uraharahussein@gmail.com>
2019-07-29 09:44:04 -0400

    added new feature

--- config.py
+++ config.py
@@ -1,19 +1,33 @@
+
 import os
-
 class Config:
-
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}&apikey={}'
+    NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     pass
 
 
 class DevConfig(Config):
-    DEBUG = True
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
 
+    DEBUG = True
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
